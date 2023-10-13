1e3ea050f9b1fc0ea25a270c596c6b1bb9dd2cc0
Raghu Kiran <Raghu@raghukiran188@gmail.com>
2023-10-13 20:38:42 +0530

    first commit

---
+++ src/Components_6/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+
+const Contact = () => {
+    return (
+        <section id="contact">
+            <div className="container">
+                <h2>Contact Me</h2>
+                <p>Email: youremail@example.com</p>
+                <p>Mobile Number: 8861513111</p>
+            </div>
+        </section>
+    );
+}
+
+export default Contact;
